0505c361e3a11b0c908bccd61a18cc5634a40054
poonam <poonamsukhdevsinghtomar@gmail.com>
2023-09-10 19:22:55 +0530

    project for conatct manager

---
+++ README.md
@@ -0,0 +1,6 @@
+## FrontEnd: https://contact-manager-007.pages.dev
+## BackEnd: https://rich-erin-springbok-garb.cyclic.cloud
+
+## Figma: https://www.figma.com/file/695hu8PptDTPJuOmdimPpX/Contacts-Manager
+
+<!-- -------------------------------------------------------------- -->
---
+++ frontend/src/components/Popup.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { FileUploader } from "react-drag-drop-files";
+import { useStateValue } from "../context/StateProvider";
+import { useState } from "react";
+import Alert from "./Alert";
+
+const Popup = ({ fetchData, setIsPop, setSuccessUp }) => {
+  // eslint-disable-next-line
+  const [state, dispatch] = useStateValue();
+  const [msg, setMsg] = useState(null);
+
+  const onTypeError = async ()=>{
+    setMsg("Please upload only CSV file");
+    setTimeout(() => {
+      setMsg(null);
+    }, 2500);
+  }
+
+  const handleChange = async (file) => {
+    const formData = new FormData();
+    formData.append("csv", file);
+    const jsonData = await fetch(process.env.REACT_APP_API + "/upload", {
+      method: "POST",
+      headers: {
+        authorization: state.user.token,
+      },
+      body: formData,
+    });
+
+    const response = await jsonData.json();
+    fetchData();
+    if (response.status === "sucess") {
+      setSuccessUp(true);
+      setTimeout(() => {
+        setSuccessUp(false);
+      }, 3000);
+    }
+    setIsPop(false);
+    console.log(response);
+  };
+  return (
+    <>    {msg ? <Alert msg={msg} /> : ""}
+    <div className="popup">
+      <FileUploader onTypeError = {onTypeError} handleChange={handleChange} name="file" types={["CSV"]} />
+    </div>
+    </>
+  );
+};
+
+export default Popup;
